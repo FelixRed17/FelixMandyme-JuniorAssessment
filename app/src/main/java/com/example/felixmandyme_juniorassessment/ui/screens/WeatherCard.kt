865d2c5829fb620b9765189b165c3b0f25469483
FelixRed17 <felix@glucode.com>
2025-06-17 18:48:09 +0200

    added a nicer weather card

--- app/src/main/java/com/example/felixmandyme_juniorassessment/ui/screens/WeatherCard.kt
+++ app/src/main/java/com/example/felixmandyme_juniorassessment/ui/screens/WeatherCard.kt
@@ -4,27 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.felixmandyme_juniorassessment.R
 import com.example.felixmandyme_juniorassessment.data.AstronomyInfo
 import com.example.felixmandyme_juniorassessment.data.WeatherInfo
 import com.example.felixmandyme_juniorassessment.ui.viewmodels.WeatherUiState
-import com.example.felixmandyme_juniorassessment.ui.viewmodels.WeatherViewModel
 
 @Composable
 fun WeatherCard(weatherUiState: WeatherUiState){
@@ -37,32 +31,44 @@ fun WeatherCard(weatherUiState: WeatherUiState){
 
 @Composable
 fun SuccessCard(weatherInfo: WeatherInfo, astronomyInfo: AstronomyInfo){
-    Card(modifier = Modifier.fillMaxWidth()){
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column {
-
-                Text(text = "${weatherInfo.current.temp} \u00B0C",
-                    style = MaterialTheme.typography.headlineLarge
-                    )
-                Text(
-                    text = "Sunset: ${astronomyInfo.astronomy.astro.sunset}"
-                )
-                Text(
-                    text = "Sunrise: ${astronomyInfo.astronomy.astro.sunrise}"
-                )
-
+    Card {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "🌅️")
+                Text(astronomyInfo.astronomy.astro.sunset)
+                Text(text = "Sunset")
             }
 
-            Spacer(modifier = Modifier.weight(1f))
-            Image(
-                painter = painterResource(R.drawable.tempicon),
-                contentDescription = "Temperature icon",
-                modifier = Modifier.size(120.dp),
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "🌡️", fontSize = 24.sp)
+                Text(
+                    text = "${weatherInfo.current.temp} \u00B0C",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
 
-
-            )
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "🌤️")
+                Text(astronomyInfo.astronomy.astro.sunrise)
+                Text(text = "Sunrise")
+            }
         }
     }
+
 }
 
 @Composable
